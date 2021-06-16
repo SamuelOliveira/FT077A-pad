b43cad2c4aa7f0f0a07685fd44f3168d79be9d0f
SamuelOliveira <samuca95@hotmail.com>
2021-06-16 21:30:30 +0000

    New test

--- MPI/RA231294/Projeto4/matrixMult.c
+++ MPI/RA231294/Projeto4/matrixMult.c
@@ -43,7 +43,7 @@ void carrega_matriz(int *args, int tamanho)
 {
     for (int i=0; i < tamanho; i++) {
         for (int j=0; j < tamanho; j++) {
-            args[posicao(i, j, tamanho)] = rand() % 10 + 1;
+            args[posicao(i, j, tamanho)] = 2;
         }
     }
 }
@@ -53,13 +53,9 @@ void print_results(char *prompt, int a[N][N]);
 int main(int argc, char *argv[])
 {
     srand(time(NULL));
-    int i, j, k, rank, size, tag = 99, blksz, sum = 0;
-/*
-    int a[N][N]={{1,2,3,4},{5,6,7,8},{9,1,2,3},{4,5,6,7,}};
-    int b[N][N]={{1,2,3,4},{5,6,7,8},{9,1,2,3},{4,5,6,7,}};
-    int c[N][N];
-    int aa[N],cc[N];
-*/
+    int i, j, k, rank, size, tag = 99, blksz, sum = 0, len;
+    char hostname[MPI_MAX_PROCESSOR_NAME];
+
     matrizA = aloca_matriz(N);
     matrizB = aloca_matriz(N);
     matrizC = aloca_matriz(N);
@@ -79,7 +75,7 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    //scatter rows of first matrix to different processes     
+    //scatter rows of first matrix to different processes
     MPI_Scatter(matrizA, N*N/size, MPI_INT, matrizP, N*N/size, MPI_INT,0,MPI_COMM_WORLD);
 
     //broadcast second matrix to all processes
@@ -87,28 +83,21 @@ int main(int argc, char *argv[])
 
     MPI_Barrier(MPI_COMM_WORLD);
 
+    //perform vector multiplication by all processes
     for (int i=0; i<N; i++)
         for (int j=0; j<N; j++)
             for (int k=0; k<N; k++)
                 matrizP[posicao(i, j, N)] += matrizA[posicao(i, k, N)] * matrizB[posicao(k, j, N)];
 
-          //perform vector multiplication by all processes
-/*
-          for (i = 0; i < N; i++)
-            {
-                    for (j = 0; j < N; j++)
-                    {
-                            sum = sum + matrizP[j] * matrizB[i][j];                
-                    }
-                    matrizD[i] = sum;
-                    sum = 0;
-            }
-*/
     MPI_Gather(matrizD, N*N/size, MPI_INT, matrizC, N*N/size, MPI_INT, 0, MPI_COMM_WORLD);
 
-    MPI_Barrier(MPI_COMM_WORLD);        
+    // Obtem o nome do processador (nome da maquina onde o processo sera executado)
+    MPI_Get_processor_name(hostname, &len);
+    printf ("Numero de tarefas = %d Meu rank= %d Rodando em %s\n", size,rank,hostname);
+
+    MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
-    //print_results("C = ", c);
+
     if(rank==0) imprime_matriz(matrizP,N);
 
     free(matrizA);
@@ -118,18 +107,3 @@ int main(int argc, char *argv[])
     free(matrizD);
 
 }
-
-void print_results(char *prompt, int a[N][N])
-{
-    int i, j;
-
-    printf ("\n\n%s\n", prompt);
-    for (i = 0; i < N; i++) {
-            for (j = 0; j < N; j++) {
-                    printf(" %d", a[i][j]);
-            }
-            printf ("\n");
-    }
-    printf ("\n\n");
-}
-
